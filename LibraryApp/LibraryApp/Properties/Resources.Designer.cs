4e41d9b188c9ab08347da84215632180748083a0
N1kase <n1kase@inbox.ru>
2025-06-12 15:53:19 +0300

    Added characters and a custom font

--- LibraryApp/LibraryApp/Properties/Resources.Designer.cs
+++ LibraryApp/LibraryApp/Properties/Resources.Designer.cs
@@ -131,6 +131,26 @@ namespace LibraryApp.Properties {
         }
         
         /// <summary>
+        ///   Поиск локализованного ресурса типа System.Byte[].
+        /// </summary>
+        internal static byte[] RuFont {
+            get {
+                object obj = ResourceManager.GetObject("RuFont", resourceCulture);
+                return ((byte[])(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Russian {
+            get {
+                object obj = ResourceManager.GetObject("Russian", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
         /// </summary>
         internal static System.Drawing.Bitmap siberian {
