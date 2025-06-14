286371b1ac1cbb9c7910d82dd5d2c7874c485ae5
exITproger <fuvfuv2024@gmail.com>
2025-06-14 20:55:22 +0300

    .

--- LibraryApp/Library_App/FareastenOmensForm.cs
+++ LibraryApp/Library_App/FareastenOmensForm.cs
@@ -26,7 +26,7 @@ namespace Library_App
 
             try
             {
-                this.BackgroundImage = Properties.Resources.пропустить;
+                this.BackgroundImage = Properties.Resources.CentralMain;
                 this.BackgroundImageLayout = ImageLayout.Zoom;
             }
             catch (Exception ex)
