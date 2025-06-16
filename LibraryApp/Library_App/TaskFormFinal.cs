0613ad7b569c8e9847ec663b23ddd9a61a6361b5
exITproger <fuvfuv2024@gmail.com>
2025-06-16 17:56:44 +0300

    Update TaskFormFinal.cs

--- LibraryApp/Library_App/TaskFormFinal.cs
+++ LibraryApp/Library_App/TaskFormFinal.cs
@@ -52,7 +52,7 @@ namespace Library_App
             // Метка с инструкцией
             Label label = new Label()
             {
-                Text = "Рсположи людей разных \n национальностей на \n соотвествующие регионы.",
+                Text = "Расположи людей разных \n национальностей на центры\n соотвествующих регионов.",
                 Font = new Font("Arial", fontSize, FontStyle.Regular),
                 AutoSize = false,
                 TextAlign = ContentAlignment.MiddleCenter,
